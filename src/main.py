1b32e5aff45e724f381d5e6b1409155719599794
DefEnge <59794331+DefEnge@users.noreply.github.com>
2023-10-27 11:43:20 +0200

    feat: introduced yaml loader for token bot

--- src/main.py
+++ src/main.py
@@ -1,1 +1,18 @@
+from telegram import Update 
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+import yaml
+from yaml.loader import SafeLoader
 
+
+with open('../config/settings.yaml', 'r',  encoding="UTF-8") as file:
+    data = yaml.load(file, Loader=SafeLoader)
+
+
+async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None: 
+    await update.message.reply_text('Hello')
+
+app = ApplicationBuilder().token(data["token"]).build()
+
+app.add_handler(CommandHandler("hello", hello))
+
+app.run_polling()
